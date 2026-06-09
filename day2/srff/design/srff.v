ee8424093920bb6d507f14dc5eaecec37486113b
Druthibiju <druthiitv123@gmail.com>
2026-06-09 20:29:11 +0530

    Update srff.v

--- day2/srff/design/srff.v
+++ day2/srff/design/srff.v
@@ -1,1 +1,31 @@
+module srff(input s,r,rst,clk,output reg q,qb );
+always@(posedge clk)
+begin
+if(rst)
+begin
+q<=1'b0;
+qb<=1'b1;
+end
+else if(s==0 && r==0)
+begin
+q<=q;
+qb<=qb;
+end
+else if(s==0 && r==1)
+begin
+q<=1'b0;
+qb<=1'b1;
+end
+else if(s==1 && r==0)
+begin
+q<=1'b1;
+qb<=1'b0;
+end
+else if(s==1&& r==1)
+begin
+q<='bx;
+qb<='bx;
+end
+end
+endmodule
 
