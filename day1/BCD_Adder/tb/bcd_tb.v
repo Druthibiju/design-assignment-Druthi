9a2a98a99f4bea21d8927ac1fc1eb69eb5b21c4b
Druthibiju <druthiitv123@gmail.com>
2026-06-09 20:43:09 +0530

    Update bcd_tb.v

--- day1/BCD_Adder/tb/bcd_tb.v
+++ day1/BCD_Adder/tb/bcd_tb.v
@@ -1,1 +1,32 @@
-
+module binary_adder_tb();
+reg [3:0]a_tb;
+reg [3:0]b_tb;
+reg cin_tb;
+wire [3:0]sum_tb;
+wire cout_tb;
+binary_adder ba1(a_tb,b_tb,cin_tb,sum_tb,cout_tb);
+initial
+begin
+{a_tb,b_tb,cin_tb}=0;
+end
+initial
+begin
+a_tb=4'b0110;
+b_tb=4'b1000;
+cin_tb=1'b1;
+#1
+a_tb=4'b1001;
+b_tb=4'b0000;
+cin_tb=1'b0;
+#1
+a_tb=4'b0011;
+b_tb=4'b1001;
+cin_tb=1'b1;
+#1
+a_tb=4'b0010;
+b_tb=4'b0001;
+cin_tb=1'b1;
+#1;
+$finish;
+end
+endmodule
