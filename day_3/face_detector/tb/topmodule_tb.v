89f098b74f39e50e18729e28a21775c7e36d7170
Druthibiju <druthiitv123@gmail.com>
2026-06-10 20:49:06 +0530

    Update topmodule_tb.v

--- day_3/face_detector/tb/topmodule_tb.v
+++ day_3/face_detector/tb/topmodule_tb.v
@@ -1,1 +1,28 @@
-
+module top_module_tb;
+reg clk_tb;
+reg rst_tb;
+reg [7:0] sin_tb;
+wire [7:0] dout_tb;
+top_module dut(.clk(clk_tb),.rst(rst_tb),.sin(sin_tb),.dout(dout_tb));
+initial
+begin
+    clk_tb = 0;
+    rst_tb = 1;
+    sin_tb = 8'h00;
+    #10 rst_tb = 0;
+end
+always #5 clk_tb = ~clk_tb;
+initial
+begin
+    #15 sin_tb = 8'h5f;
+    #10 sin_tb = 8'h21;
+    #10 sin_tb = 8'h10;
+    #10 sin_tb = 8'h26;
+    #10 sin_tb = 8'h1c;
+    #10 sin_tb = 8'h02;
+    #10 sin_tb = 8'h2b;
+    #10 sin_tb = 8'ha1;
+    #200;
+    $finish;
+end
+endmodule
