5baa1f1542067bda15c96827275e86167b9d2036
Druthibiju <druthiitv123@gmail.com>
2026-06-10 20:52:51 +0530

    Update top_module.v

--- day_3/face_detector/Design/top_module.v
+++ day_3/face_detector/Design/top_module.v
@@ -1,1 +1,12 @@
+module top_module(input clk, rst,input [7:0] sin,output [7:0] dout);
 
+wire [7:0] sout;
+wire [7:0] fifo_out;
+wire full, empty;
+wire rd_enb;
+face_mod fm1(clk,sin,sout);
+out_module om1(clk,rst,rd_enb);
+fifo ff1(clk,rst,1'b1,rd_enb,sout,full,empty,fifo_out);
+assign dout = fifo_out;
+
+endmodule
